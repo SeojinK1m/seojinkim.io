505b76d24b621d57cbecca7164ff3f808e2565e0
Seojin Kim <skim27@wustl.edu>
2020-07-30 21:03:08 -0700

    my website

---
+++ sam.js
@@ -0,0 +1,16 @@
+function c(){
+    alert("The C Programming Language")
+}
+function python(){
+    alert("The Python Programming Language")
+}
+function java(){
+    alert("The Java Programming Language")
+}
+function js(){
+    alert("The JavaScript Scripting Language")
+}
+
+function scrollMe(){
+    document.querySelector(".sam").scrollIntoView();
+}
